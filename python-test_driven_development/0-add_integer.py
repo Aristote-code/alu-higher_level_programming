aafb1831504427bc07171c5a78a09d6dd64002f3
Aristote-code <gahimaaristote1@gmail.com>
2023-04-20 13:57:24 +0200

    Updating file 0-add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,12 +2,13 @@
 
 ''' Integer addition exception of float'''
 
+
 def add_integer(a, b=98):
+    ''' A function that adds Integers with an exception of float'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    return (int(a) + int(b))
 
+    return (int(a) + int(b))
