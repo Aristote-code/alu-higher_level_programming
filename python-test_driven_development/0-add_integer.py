1725eecaf5f2687112d11e59d1b2080a36fe8d52
Aristote-code <gahimaaristote1@gmail.com>
2023-04-20 13:46:46 +0200

    updating 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a and b must be integers or floats") 
+        raise TypeError("a and b must be integers or floats")
 
     return (int(a) + int(b))
 
